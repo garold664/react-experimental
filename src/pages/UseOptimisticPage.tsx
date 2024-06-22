e090799ecbd2501a43795db086231acc1fe657d5
garold664 <garold664@gmail.com>
2024-06-22 12:18:50 +0300

    uses action instead of startTransition

--- src/pages/UseOptimisticPage.tsx
+++ src/pages/UseOptimisticPage.tsx
@@ -15,13 +15,19 @@ export default function UseOptimisticPage() {
     }
   );
 
-  const onSubmit = () => {
-    startTransition(async () => {
-      addOptimisticTodo(text);
-      await timerPromise(2000);
-      setTodos((prevState) => {
-        return [...prevState, text];
-      });
+  const onSubmit = async (formData: FormData) => {
+    // startTransition(async () => {
+    //   addOptimisticTodo(text);
+    //   await timerPromise(2000);
+    //   setTodos((prevState) => {
+    //     return [...prevState, text];
+    //   });
+    // });
+
+    addOptimisticTodo(formData.get('text') as string);
+    await timerPromise(2000);
+    setTodos((prevState) => {
+      return [...prevState, text];
     });
   };
   return (
@@ -29,12 +35,16 @@ export default function UseOptimisticPage() {
       <h1>UseOptimisticPage</h1>
       <h2>Start editing to see some magic happen!</h2>
 
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        type="text"
-      />
-      <button onClick={onSubmit}>add todo</button>
+      <form action={onSubmit}>
+        <input
+          name="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+        />
+        {/* <button onClick={onSubmit}>add todo</button> */}
+        <button>add todo</button>
+      </form>
       <ul>
         {optimisticTodos.map((todo) => (
           <li key={todo}>{todo}</li>
