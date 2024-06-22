162b7731017cd40e75c7d37c1d93dd9e4025d629
garold664 <garold664@gmail.com>
2024-06-22 19:58:23 +0300

    adds useTransition to fix a problem

--- src/pages/UseTransition.tsx
+++ src/pages/UseTransition.tsx
@@ -1,28 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useTransition } from 'react';
 
-const LIST_SIZE = 3 * 10 ** 4;
+const LIST_SIZE = 1 * 10 ** 4;
 
 export default function UseTransition() {
+  const [isPending, startTransition] = useTransition();
   const [input, setInput] = useState('');
-  const [list, setList] = useState<number[]>([]);
+  const [list, setList] = useState<string[]>([]);
+
+  // function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  //   //# here we are setting 2 states at the same time
+  //   // by default states are updated in batch.
+  //   // because of that input state gots delayed
+  //   setInput(e.target.value);
+  //   const l: string[] = [];
+  //   for (let i = 0; i < LIST_SIZE; i++) {
+  //     l.push(e.target.value);
+  //   }
+  //   setList(l);
+  // }
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    //# here we are setting 2 states at the same time
-    // by default states are updated in batch.
-    // because of that input state gots delayed
     setInput(e.target.value);
-    const l: number[] = [];
-    for (let i = 0; i < LIST_SIZE; i++) {
-      l.push(+e.target.value);
-    }
-    setList(l);
+    startTransition(() => {
+      //# this code has lower priority
+      const l: string[] = [];
+      for (let i = 0; i < LIST_SIZE; i++) {
+        l.push(e.target.value);
+      }
+      setList(l);
+    });
   }
   return (
     <>
       <h1>UseTransition</h1>
       <input type="text" value={input} onChange={handleChange} />
-      {list.map((item: number, index: number) => (
-        <div key={index}>{item}</div>
-      ))}
+      {isPending ? (
+        <div>Loading...</div>
+      ) : (
+        list.map((item: string, index: number) => <div key={index}>{item}</div>)
+      )}
     </>
   );
 }
