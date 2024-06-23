b2c48e709938f8d2acc29834564eede6f98bd496
garold664 <garold664@gmail.com>
2024-06-23 21:41:27 +0300

    applies useImperativeHandle to expose extra methods to parent component

--- src/pages/UseImperativeHandle2/ConfirmationModal.tsx
+++ src/pages/UseImperativeHandle2/ConfirmationModal.tsx
@@ -1,28 +1,46 @@
-import React, {
-  ComponentPropsWithoutRef,
-  forwardRef,
-  useImperativeHandle,
-} from 'react';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import { RefMethods } from './UseImperativeHandle2';
 
 type ModalProps = { isOpen: boolean; onClose: () => void };
 
-export default forwardRef<HTMLDivElement, ModalProps>(
-  function ConfirmationModal({ isOpen, onClose }: ModalProps, ref) {
-    if (!isOpen) {
-      return null;
-    }
-    return (
-      <div className="modal" ref={ref}>
-        <button>&times;</button>
-        <header className="modal-header">
-          <h2>Title</h2>
-        </header>
-        <div className="modal-body">Do you confirm?</div>
-        <footer className="modal-footer">
-          <button onClick={onClose}>Yes</button>
-          <button onClick={onClose}>No</button>
-        </footer>
-      </div>
-    );
+export default forwardRef<RefMethods, ModalProps>(function ConfirmationModal(
+  { isOpen, onClose }: ModalProps,
+  ref
+) {
+  const closeRef = useRef<HTMLButtonElement>(null);
+  const denyRef = useRef<HTMLButtonElement>(null);
+  const confirmRef = useRef<HTMLButtonElement>(null);
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        focusCloseBtn: () => closeRef.current?.focus(),
+        focusDenyBtn: () => denyRef.current?.focus(),
+        focusConfirmBtn: () => confirmRef.current?.focus(),
+      };
+    },
+    []
+  );
+
+  if (!isOpen) {
+    return null;
   }
-);
+  return (
+    <div className="modal">
+      <button ref={closeRef}>&times;</button>
+      <header className="modal-header">
+        <h2>Title</h2>
+      </header>
+      <div className="modal-body">Do you confirm?</div>
+      <footer className="modal-footer">
+        <button ref={confirmRef} onClick={onClose}>
+          Yes
+        </button>
+        <button ref={denyRef} onClick={onClose}>
+          No
+        </button>
+      </footer>
+    </div>
+  );
+});
--- src/pages/UseImperativeHandle2/UseImperativeHandle2.tsx
+++ src/pages/UseImperativeHandle2/UseImperativeHandle2.tsx
@@ -1,16 +1,28 @@
 import React, { useRef, useState } from 'react';
 import ConfirmationModal from './ConfirmationModal';
 
+export type RefMethods = {
+  focusCloseBtn: () => void;
+  focusDenyBtn: () => void;
+  focusConfirmBtn: () => void;
+};
+
 export default function UseImperativeHandle2() {
   const [open, setOpen] = useState(false);
-  const modalRef = useRef<HTMLDivElement>(null);
+  const modalRef = useRef<RefMethods>(null);
   return (
     <>
       <h1>UseImperativeHandle 2nd example</h1>
       <button onClick={() => setOpen(true)}>Open</button>
-      <button>Focus Close</button>
-      <button>Focus Confirm</button>
-      <button>Focus Deny</button>
+      <button onClick={() => modalRef.current?.focusCloseBtn()}>
+        Focus Close
+      </button>
+      <button onClick={() => modalRef.current?.focusConfirmBtn()}>
+        Focus Confirm
+      </button>
+      <button onClick={() => modalRef.current?.focusDenyBtn()}>
+        Focus Deny
+      </button>
       <ConfirmationModal
         ref={modalRef}
         isOpen={open}
