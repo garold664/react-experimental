82098e2b356069044fbbc4346f1efc6b78109c63
garold664 <garold664@gmail.com>
2024-06-23 15:18:54 +0300

    adds useDefferedValue hook to fix the problem

--- src/pages/UseDefferedValue/List.tsx
+++ src/pages/UseDefferedValue/List.tsx
@@ -1,13 +1,20 @@
-import React, { useMemo } from 'react';
+import React, { useDeferredValue, useMemo } from 'react';
 
-const LIST_SIZE = 1 * 10 ** 4;
+const LIST_SIZE = 2 * 10 ** 4;
 export default function List({ input }: { input: string }) {
+  const defferedInput = useDeferredValue(input);
+  //# useDeferredValue immediately returns previous value and then waits until changes stop happening and only then returns new value
+  //# in other words useDefferedvalue makes this value of low priority
+
+  //# the value can come from anywhere (state, some library, api, ...)
+  console.log('input: ', input);
+  console.log('defferedInput: ', defferedInput);
   const list = useMemo(() => {
     const l: JSX.Element[] = [];
     for (let i = 0; i < LIST_SIZE; i++) {
-      l.push(<div key={i}>{input}</div>);
+      l.push(<div key={i}>{defferedInput}</div>);
     }
     return l;
-  }, [input]);
+  }, [defferedInput]);
   return list;
 }
